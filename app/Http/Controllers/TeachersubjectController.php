dc6244baf8af3a5c9083641bd2976d39d6af573f
w <eropa@live.ru>
2020-02-07 02:45:57 +0200

    dnevnik(1)

---
+++ app/Http/Controllers/TeachersubjectController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Teachersubject;
+use Illuminate\Http\Request;
+
+class TeachersubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Teachersubject  $teachersubject
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Teachersubject $teachersubject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Teachersubject  $teachersubject
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Teachersubject $teachersubject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Teachersubject  $teachersubject
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Teachersubject $teachersubject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Teachersubject  $teachersubject
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Teachersubject $teachersubject)
+    {
+        //
+    }
+}
---
+++ database/factories/TeachersubjectFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Teachersubject;
+use Faker\Generator as Faker;
+
+$factory->define(Teachersubject::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
